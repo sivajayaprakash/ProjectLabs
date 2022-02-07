612f1a9e8b41e8e1c23fffc602ef2fb982fa8c4e
sivajayaprakash <sivajayaprakash@gmail.com>
2022-02-07 20:01:12 +0530

    Created Procfile for Heroku

--- index.js
+++ index.js
@@ -6,8 +6,36 @@ const app = express();
 
 const path = require('path');
 
+const request = require('request');
+
+const bodyParser =require('body-parser');
+
 
 const PORT =  process.env.PORT || 8000;
+
+//use body parser middleware
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+
+
+//API pk_d91db140673041d38de5e2caf74067aa
+//Create Call API function
+
+function call_api(finishedAPI) {
+request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_d91db140673041d38de5e2caf74067aa', {json: true}, (err, res, body) => {
+
+	if(err) {return console.log (err);}
+
+		if (res.statusCode === 200) {
+		//console.log(body);
+		finishedAPI(body);
+	};
+});
+
+};
+
+
 
 //Set Handlebars Middleware
 
@@ -15,11 +43,42 @@ app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 
 
-//Set handlebar routes
+const otherstuff ="hello there, this is other stuff";
+
+
+//Set handlebar index GET route
 
 app.get('/', function (req, res){
-	res.render('home');
+	call_api(function(doneAPI) {
+
+	res.render('home', {
+		stock:doneAPI
+	});
+		
+});
+
+});
+
+//Set handlebar index POST route
+
+app.get('/', function (req, res){
+	call_api(function(doneAPI) {
+		
+	res.render('home', {
+		stock:doneAPI,
+		
+	});
+		
+});
+
+});
+
+//Set handlebar routes
+
+app.get('/about.html', function (req, res){
+	res.render('about');
 })
+
 
 
 //Set Static Folder
