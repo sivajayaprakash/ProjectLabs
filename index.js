017ab63cdcee56ce3aff18224566dfd073c4db54
sivajayaprakash <sivajayaprakash@gmail.com>
2022-02-22 23:51:38 +0530

    Second Commit

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 const express = require('express');
-
 const exphbs = require('express-handlebars');
+const path = require('path');
+const request = require('request');
+const bodyParser =require('body-parser');
 
 const app = express();
 
-const path = require('path');
-
-const request = require('request');
-
-const bodyParser =require('body-parser');
-
-
 const PORT =  process.env.PORT || 8000;
 
-//use body parser middleware
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended:false}));
-
-
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+app.use(express.urlencoded({extended: true}));
 
 //API pk_d91db140673041d38de5e2caf74067aa
 //Create Call API function
 
+var stockticker = 'goog';
+
+
 function call_api(finishedAPI) {
-request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_d91db140673041d38de5e2caf74067aa', {json: true}, (err, res, body) => {
+request(`https://cloud.iexapis.com/stable/stock/${stockticker}/quote?token=pk_d91db140673041d38de5e2caf74067aa`, {json: true}, (err, res, body) => {
 
 	if(err) {return console.log (err);}
 
 		if (res.statusCode === 200) {
-		//console.log(body);
+		// console.log(body);
 		finishedAPI(body);
 	};
 });
@@ -36,53 +34,59 @@ request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_d91db140673041
 };
 
 
-
-//Set Handlebars Middleware
-
-app.engine('handlebars', exphbs.engine());
-app.set('view engine', 'handlebars');
-
-
-const otherstuff ="hello there, this is other stuff";
-
-
 //Set handlebar index GET route
 
 app.get('/', function (req, res){
-	call_api(function(doneAPI) {
-
-	res.render('home', {
-		stock:doneAPI
+	call_api(function(doneAPII) {
+		res.render('home', {
+		stock:doneAPII
+		});
 	});
-		
 });
 
-});
-
-//Set handlebar index POST route
-
-app.get('/', function (req, res){
-	call_api(function(doneAPI) {
-		
-	res.render('home', {
-		stock:doneAPI,
-		
-	});
-		
-});
-
-});
 
 //Set handlebar routes
 
-app.get('/about.html', function (req, res){
-	res.render('about');
+app.get('/about', function (req, res){
+
+	call_api(function(doneAPII) {
+		res.render('about', {
+		stock:doneAPII
+		});
+	});
+	
 })
 
 
 
-//Set Static Folder
+app.get('/livealerts', function (req, res){
 
-app.use(express.static(path.join(__dirname, 'public')));
+	call_api(function(doneAPII) {
+		res.render('livealerts', {
+		stock:doneAPII
+		});
+	});
+	
+})
+
+
+
+app.get('/login', function (req, res){
+
+	call_api(function(doneAPII) {
+		res.render('login', {
+		stock:doneAPII
+		});
+	});
+	
+})
+
+
+// app.post('/submit-form', (req, res) => {
+// 	const stock_ticker = req.body.stock_ticker;
+// 	//...
+// 	res.end()
+//   })
+
 
 app.listen(PORT, () => console.log('Server Listening on Port' +  PORT ));
